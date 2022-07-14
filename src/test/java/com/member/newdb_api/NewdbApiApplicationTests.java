292abe598eb6e932a704d73f2b5dcb59fe145693
douja123 <103461652+douja123@users.noreply.github.com>
2022-07-13 23:50:52 -0700

    Add files via upload

---
+++ src/test/java/com/member/newdb_api/NewdbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.member.newdb_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewdbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
